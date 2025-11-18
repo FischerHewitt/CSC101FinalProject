f47c78dcb0ccc8bf2a8fff994246037521ee42ae
fischerhewitt <fihewitt@calpoly.edu>
2025-11-18 09:50:14 -0800

    data should be complete

--- transitfunctions.py
+++ transitfunctions.py
@@ -1,5 +1,5 @@
 from data import Transportations
 
-
+# (kWh/mile) x (kgCO2/kWh) = (kgCO2/mile)
 def get_co2_per_vehicle_mile(mode:Transportations) -> float:
     return mode.energy['value'] * mode.emissions['factor']
