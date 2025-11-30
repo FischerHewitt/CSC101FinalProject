5e9d5fe76f0a18129a0450bf945f4f22d62fff6d
fischerhewitt <fihewitt@calpoly.edu>
2025-11-30 15:57:08 -0800

    data is complete

--- test_cases.py
+++ test_cases.py
@@ -29,7 +29,7 @@ class UnitTests(unittest.TestCase):
     def test_get_city_co2_per_day(self):
         input1 = full_data
         actual = transitfunctions.get_city_co2_per_day(input1)
-        expected = {'New York City': 1646340.0885, 'Chicago': 584325.26, 'Seattle': 295829.04,
+        expected = {'New York City': 1646340.0885, 'Chicago': 584325.26, 'Seattle': 295409.83999999997,
                     'Los Angeles': 805112.167, 'San Francisco': 289827.66000000003}
         self.assertEqual(actual, expected)
         print("Unsorted:", actual)
@@ -38,7 +38,7 @@ class UnitTests(unittest.TestCase):
         input1 = full_data
         input2 = transitfunctions.get_city_co2_per_day(input1)
         sort = transitfunctions.sort_city_co2_per_day(input2)
-        expected = {'San Francisco': 289827.66000000003, 'Seattle': 295829.04, 'Chicago': 584325.26,
+        expected = {'San Francisco': 289827.66000000003, 'Seattle': 295409.83999999997, 'Chicago': 584325.26,
                     'Los Angeles': 805112.167, 'New York City': 1646340.0885}
         self.assertEqual(sort, expected)
         print("Sorted:", sort)
@@ -47,7 +47,7 @@ class UnitTests(unittest.TestCase):
         input1 = full_data
         actual = transitfunctions.get_mode_metric_co2_per_day(input1)
         expected = {'Bus-diesel': 2163998.7199999997, 'Heavy Rail-diesel': 25501.6,
-                    'Heavy Rail-electric': 519218.67299999995, 'Light Rail-electric': 912715.2225000001}
+                    'Heavy Rail-electric': 519218.67299999995, 'Light Rail-electric': 912296.0225000002}
         self.assertEqual(actual, expected)
         print("Unsorted:", actual)
 
@@ -56,7 +56,7 @@ class UnitTests(unittest.TestCase):
         input2 = transitfunctions.get_mode_metric_co2_per_day(input1)
         sort = transitfunctions.sort_mode_metric_co2_per_day(input2)
         expected = {'Heavy Rail-diesel': 25501.6, 'Heavy Rail-electric': 519218.67299999995,
-                    'Light Rail-electric': 912715.2225000001, 'Bus-diesel': 2163998.7199999997}
+                    'Light Rail-electric': 912296.0225000002, 'Bus-diesel': 2163998.7199999997}
         self.assertEqual(sort, expected)
         print("Sorted:", sort)
 
